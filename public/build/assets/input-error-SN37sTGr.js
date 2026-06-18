08bc5af8b8f6f7a58b04629a2aa9d4e1f8c87313
Anwar Shaikh <anwar.squarebiz@gmail.com>
2026-06-18 11:06:36 +0530

    build the frontend

---
+++ public/build/assets/input-error-SN37sTGr.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DwLt1bCy.js";import{c as o}from"./utils-BB6yRsNU.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
